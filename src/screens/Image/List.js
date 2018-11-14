1a60c12dacf8861fcd08747bbc5cb6d653c2b1a2
Alex McCabe <alexwmccabe@gmail.com>
2018-11-14 16:08:15 +0000

    Fix: FilterMenu sitting above images

--- src/screens/Image/List.js
+++ src/screens/Image/List.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { Component, createRef } from 'react';
+import Measure from 'react-measure';
 
-import ImageFilterBar from 'components/Image/FilterBar/FilterBar';
+import ImageFilterMenu from 'components/Image/FilterMenu/FilterMenu';
 import ImageListContainer from 'components/Image/List/ListContainer';
+import { setStyle } from 'utils';
 
-const ScreensImageList = () => {
-  return (
-    <div id="screen-image-list">
-      <ImageFilterBar />
-      <ImageListContainer />
-    </div>
-  );
-};
+import { filterMenuItems } from 'config';
+
+class ScreensImageList extends Component {
+  containerRef = createRef();
+  state = {
+    filterMenuHeight: 0
+  };
+
+  onMeasureResize = ({ bounds }) => {
+    const { height } = bounds;
+
+    if (height !== this.state.filterMenuHeight) {
+      setStyle(this.containerRef.current, { paddingTop: `${height}px` });
+
+      this.setState({ filterMenuHeight: height });
+    }
+  };
+
+  render() {
+    return (
+      <div id="screen-image-list" ref={this.containerRef}>
+        <Measure bounds onResize={this.onMeasureResize}>
+          {({ measureRef }) => {
+            return (
+              <ImageFilterMenu ref={measureRef} filters={filterMenuItems} />
+            );
+          }}
+        </Measure>
+        <ImageListContainer />
+      </div>
+    );
+  }
+}
 
 export default ScreensImageList;
