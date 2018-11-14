824cb74d841bd54a432cf6395e07ddd809c5fcb1
Alex McCabe <alexwmccabe@gmail.com>
2018-11-14 16:06:46 +0000

    Add: Filter menu items config

---
+++ src/config/filterMenuItems.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    label: 'Latest',
+    url: '/',
+    exact: true
+  },
+  {
+    label: 'Popular',
+    url: '/popular',
+    exact: false
+  },
+  {
+    label: 'Oldest',
+    url: '/oldest',
+    exact: false
+  }
+];
